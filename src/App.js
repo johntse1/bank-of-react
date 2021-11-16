55c15c0083000c07d24b3d1892f4ac640e29d4c6
John Tse <jtse63@gmail.com>
2021-11-16 01:43:26 -0500

    Initial commit
    This is the initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
+  }
+
+  constructor(){
+    super();
+    
+    this.state = {
+      accountBalance: 14568.27, 
+      currentUser:{
+        userName: 'joe_shmo',
+        memberSince: '07/23/96',
+      }
+    }
+  }
+  render() {
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent =() =>{
+      <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
+    };
+    return (
+      <Router>
+      <div>
+        <Route exact path ="/" render={HomeComponent}/>
+        <Route exact path ="/userProfile" render={UserProfileComponent}/>
+        <Route exact path="/login" render={LogInComponent}/>
+      </div>
+    </Router>
+    );
+  }
 }
 
 export default App;
